32ff8726998cd1457b75c784298c5111d9688595
Victor <victorsmirnov@me.com>
2020-08-30 17:35:47 +0300

    add mail services

--- config.py
+++ config.py
@@ -9,11 +9,15 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{os.path.join(base_dir, "app.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'victorsmirnov67@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'victorS77'
     ADMINS = ['victorsmirnov67@gmail.com']
+
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS' or True)
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL' or True)
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME' or 'victorsmirnov67@gmail.com')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD' or 'victorS77')
+    MAIL_DEFAULT_SENDER = ADMINS[0]
 
     POSTS_PER_PAGE = 25

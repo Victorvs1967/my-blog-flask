121a3db5bb35d38835e5c40dfa7bfc02c2d70772
Victor <victorsmirnov@me.com>
2020-08-31 13:41:09 +0300

    add some franworks

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+babel = Babel(app)
 bootstrap = Bootstrap(app)
 
 if not app.debug:
